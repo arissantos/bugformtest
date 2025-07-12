36edcb188f6bb694c86d1f16abe71c22309f2a83
Aris Santos <arissantos@yahoo.com>
2025-07-12 12:08:24 +1200

    set .gitignore

---
+++ README.md
@@ -0,0 +1,21 @@
+// Steps to Run the Test
+
+// Prerequisites
+
+- Ensure you have [Node.js](https://nodejs.org/) and [npm](https://www.npmjs.com/get-npm) installed.
+
+// Installation
+
+1. Open your terminal and navigate to your project directory.
+2. Install Playwright and Faker as development dependencies:
+    ```bash
+    npm install -D @playwright/test
+    npm install --save-dev playwright @faker-js/faker
+    ```
+
+// Running Tests
+
+Execute the following command to run your Playwright tests:
+```bash
+npx playwright test
+```
--- tests/bugFormtest.spec.ts
+++ tests/bugFormtest.spec.ts
@@ -29,7 +29,7 @@ async function user(page: Page, overrides = {}) {
 
 test('Successful registration', async ({ page }) => {
   const bugForm = await user(page)
-  const notification = page.getByText('Successfully registered the');
+  const notification = page.locator('#message');
   // Fill in the form fields
   await page.getByRole('textbox', { name: 'First Name' }).fill(bugForm.fname)
   await page.getByRole('textbox', { name: 'Last Name* Phone nunber*' }).fill(bugForm.lname)
@@ -38,12 +38,18 @@ test('Successful registration', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Enter email' }).fill(bugForm.emailAddress)
   await page.getByRole('textbox', { name: 'Password' }).fill(bugForm.password)
   await page.getByRole('button', { name: 'Register' }).click();
+  const checkbox = page.getByRole('checkbox', { name: 'I agree with the terms and' });
+  if (await checkbox.isEnabled()) {
+    await checkbox.check();
+  } else {
+    console.error('Checkbox is disabled');
+  }
   await expect(notification).toHaveText("Successfully registered the following information");
 });
 
-test('Required field:Lastname', async ({ page }) => {
+test('Required field: Lastname', async ({ page }) => {
   const bugForm = await user(page)
-  const notification = page.getByText('Successfully registered the');
+  const notification = page.locator('#message');
   // Fill in the form fields
   await page.getByRole('textbox', { name: 'First Name' }).fill(bugForm.fname)
   // await page.getByRole('textbox', { name: 'Last Name* Phone nunber*' }).fill(bugForm.lname)
@@ -52,11 +58,17 @@ test('Required field:Lastname', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Enter email' }).fill(bugForm.emailAddress)
   await page.getByRole('textbox', { name: 'Password' }).fill(bugForm.password)
   await page.getByRole('button', { name: 'Register' }).click();
+  const checkbox = page.getByRole('checkbox', { name: 'I agree with the terms and' });
+  if (await checkbox.isEnabled()) {
+    await checkbox.check();
+  } else {
+    console.error('Checkbox is disabled');
+  }
   await expect(notification).toHaveText("Missing Lastname");
 });
-test('Required field:PhoneNumber', async ({ page }) => {
+test('Required field: PhoneNumber', async ({ page }) => {
   const bugForm = await user(page)
-  const notification = page.getByText('Successfully registered the');
+  const notification = page.locator('#message');
   // Fill in the form fields
   await page.getByRole('textbox', { name: 'First Name' }).fill(bugForm.fname)
   await page.getByRole('textbox', { name: 'Last Name* Phone nunber*' }).fill(bugForm.lname)
@@ -65,11 +77,17 @@ test('Required field:PhoneNumber', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Enter email' }).fill(bugForm.emailAddress)
   await page.getByRole('textbox', { name: 'Password' }).fill(bugForm.password)
   await page.getByRole('button', { name: 'Register' }).click();
+  const checkbox = page.getByRole('checkbox', { name: 'I agree with the terms and' });
+  if (await checkbox.isEnabled()) {
+    await checkbox.check();
+  } else {
+    console.error('Checkbox is disabled');
+  }
   await expect(notification).toHaveText("Missing PhoneNumber");
 });
 test('PhoneNumber: Should only allow number', async ({ page }) => {
   const bugForm = await user(page)
-  const notification = page.getByText('Successfully registered the');
+  const notification = page.locator('#message');
   // Fill in the form fields
   await page.getByRole('textbox', { name: 'First Name' }).fill(bugForm.fname)
   await page.getByRole('textbox', { name: 'Last Name* Phone nunber*' }).fill(bugForm.lname)
@@ -78,11 +96,17 @@ test('PhoneNumber: Should only allow number', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Enter email' }).fill(bugForm.emailAddress)
   await page.getByRole('textbox', { name: 'Password' }).fill(bugForm.password)
   await page.getByRole('button', { name: 'Register' }).click();
+  const checkbox = page.getByRole('checkbox', { name: 'I agree with the terms and' });
+  if (await checkbox.isEnabled()) {
+    await checkbox.check();
+  } else {
+    console.error('Checkbox is disabled');
+  }
   await expect(notification).toHaveText("Invalid PhoneNumber");
 });
-test('PhoneNumber: Less than 10 digits', async ({ page }) => {
+test('PhoneNumber: Should not be less than 10 digits', async ({ page }) => {
   const bugForm = await user(page)
-  const notification = page.getByText('Successfully registered the');
+  const notification = page.locator('#message');
   // Fill in the form fields
   await page.getByRole('textbox', { name: 'First Name' }).fill(bugForm.fname)
   await page.getByRole('textbox', { name: 'Last Name* Phone nunber*' }).fill(bugForm.lname)
@@ -91,11 +115,25 @@ test('PhoneNumber: Less than 10 digits', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Enter email' }).fill(bugForm.emailAddress)
   await page.getByRole('textbox', { name: 'Password' }).fill(bugForm.password)
   await page.getByRole('button', { name: 'Register' }).click();
-  await expect(notification).toHaveText("Invalid PhoneNumber");
+  const checkbox = page.getByRole('checkbox', { name: 'I agree with the terms and' });
+  if (await checkbox.isEnabled()) {
+    await checkbox.check();
+  } else {
+    console.error('Checkbox is disabled');
+  }
+  await expect(notification).toHaveText("The phone number should contain at least 10 characters!");
 });
-test('Required field:Email Address', async ({ page }) => {
+test('PhoneNumber: Should display correct field name', async ({ page }) => {
   const bugForm = await user(page)
-  const notification = page.getByText('Successfully registered the');
+  const notification = page.locator('#message');
+  // Fill in the form fields
+  const phoneNumberField = page.getByText('Phone nunber*');
+  await expect(phoneNumberField).toHaveText("Phone number*");
+});
+
+test('Required field: Email Address', async ({ page }) => {
+  const bugForm = await user(page)
+  const notification = page.locator('#message');
   // Fill in the form fields
   await page.getByRole('textbox', { name: 'First Name' }).fill(bugForm.fname)
   await page.getByRole('textbox', { name: 'Last Name* Phone nunber*' }).fill(bugForm.lname)
@@ -104,6 +142,12 @@ test('Required field:Email Address', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Enter email' }).fill(bugForm.emailAddress)
   await page.getByRole('textbox', { name: 'Password' }).fill(bugForm.password)
   await page.getByRole('button', { name: 'Register' }).click();
+  const checkbox = page.getByRole('checkbox', { name: 'I agree with the terms and' });
+  if (await checkbox.isEnabled()) {
+    await checkbox.check();
+  } else {
+    console.error('Checkbox is disabled');
+  }
   try {
     await expect(notification).toHaveText("Missing Email Address");
   } catch (error) {
@@ -111,9 +155,9 @@ test('Required field:Email Address', async ({ page }) => {
     throw error;
   }
 });
-test('Email Address:should have @ sign ', async ({ page }) => {
+test('Email Address: Should have @ sign ', async ({ page }) => {
   const bugForm = await user(page)
-  const notification = page.getByText('Successfully registered the');
+  const notification = page.locator('#message');
   // Fill in the form fields
   await page.getByRole('textbox', { name: 'First Name' }).fill(bugForm.fname)
   await page.getByRole('textbox', { name: 'Last Name* Phone nunber*' }).fill(bugForm.lname)
@@ -122,6 +166,12 @@ test('Email Address:should have @ sign ', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Enter email' }).fill("testemail.com") // Missing @ sign
   await page.getByRole('textbox', { name: 'Password' }).fill(bugForm.password)
   await page.getByRole('button', { name: 'Register' }).click();
+  const checkbox = page.getByRole('checkbox', { name: 'I agree with the terms and' });
+  if (await checkbox.isEnabled()) {
+    await checkbox.check();
+  } else {
+    console.error('Checkbox is disabled');
+  }
   try {
     await expect(notification).toHaveText("Invalid Email Address");
   } catch (error) {
@@ -131,7 +181,7 @@ test('Email Address:should have @ sign ', async ({ page }) => {
 });
 test('Required: Checkbox', async ({ page }) => {
   const bugForm = await user(page)
-  const notification = page.getByText('Successfully registered the');
+  const notification = page.locator('#message');
   // Fill in the form fields
   await page.getByRole('textbox', { name: 'First Name' }).fill(bugForm.fname)
   await page.getByRole('textbox', { name: 'Last Name* Phone nunber*' }).fill(bugForm.lname)
@@ -146,9 +196,63 @@ test('Required: Checkbox', async ({ page }) => {
     await checkbox.check();
   } else {
     console.error('Checkbox is disabled');
-  }  
+  }
   try {
-    await expect(notification).toHaveText("Successfully registered the following information");
+    await expect(notification).toHaveText("I agree with the terms and conditions checkbox is required");
+  } catch (error) {
+    console.error('Test failed:', error);
+    throw error;
+  }
+});
+test('Password: Should not be less than 6 characters  ', async ({ page }) => {
+  const bugForm = await user(page)
+  const notification = page.locator('#message');
+  // Fill in the form fields
+  await page.getByRole('textbox', { name: 'First Name' }).fill(bugForm.fname)
+  await page.getByRole('textbox', { name: 'Last Name* Phone nunber*' }).fill(bugForm.lname)
+  await page.getByRole('textbox', { name: 'Enter phone number' }).fill(bugForm.pNumber)
+  await page.locator('#countries_dropdown_menu').selectOption(bugForm.country);
+  await page.getByRole('textbox', { name: 'Enter email' }).fill("testemail.com") // Missing @ sign
+  await page.getByRole('textbox', { name: 'Password' }).fill("12345")
+  await page.getByRole('button', { name: 'Register' }).click();
+
+  const checkbox = page.getByRole('checkbox', { name: 'I agree with the terms and' });
+  if (await checkbox.isEnabled()) {
+    await checkbox.check();
+  } else {
+    console.error('Checkbox is disabled');
+  }
+
+  await page.getByText('The password should contain').click();
+  try {
+    await expect(notification).toHaveText("The password should contain between [6,20] characters!");
+  } catch (error) {
+    console.error('Test failed:', error);
+    throw error;
+  }
+});
+test('Password: Should not be more than 20 characters  ', async ({ page }) => {
+  const bugForm = await user(page)
+  const notification = page.locator('#message');
+  // Fill in the form fields
+  await page.getByRole('textbox', { name: 'First Name' }).fill(bugForm.fname)
+  await page.getByRole('textbox', { name: 'Last Name* Phone nunber*' }).fill(bugForm.lname)
+  await page.getByRole('textbox', { name: 'Enter phone number' }).fill(bugForm.pNumber)
+  await page.locator('#countries_dropdown_menu').selectOption(bugForm.country);
+  await page.getByRole('textbox', { name: 'Enter email' }).fill("testemail.com") // Missing @ sign
+  await page.getByRole('textbox', { name: 'Password' }).fill("asdasd23a123dsdasdasd231sd23a1sd1as23d1a")
+  await page.getByRole('button', { name: 'Register' }).click();
+
+  const checkbox = page.getByRole('checkbox', { name: 'I agree with the terms and' });
+  if (await checkbox.isEnabled()) {
+    await checkbox.check();
+  } else {
+    console.error('Checkbox is disabled');
+  }
+
+  await page.getByText('The password should contain').click();
+  try {
+    await expect(notification).toHaveText("The password should contain between [6,20] characters!");
   } catch (error) {
     console.error('Test failed:', error);
     throw error;
