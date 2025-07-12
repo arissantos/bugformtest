a31f4601b266b7b53e657b879c7bc411982c08aa
Aris Santos <arissantos@yahoo.com>
2025-07-12 12:15:11 +1200

    clean up code

--- tests/bugFormtest.spec.ts
+++ tests/bugFormtest.spec.ts
@@ -3,15 +3,6 @@ import { faker } from '@faker-js/faker'
 
 
 async function user(page: Page, overrides = {}) {
-
-
-  // const fName = this.page.getByRole('textbox', { name: 'First Name' });
-  // const lName = this.page.getByRole('textbox', { name: 'Last Name* Phone nunber*' });
-  // const pNumber = this.page.getByRole('textbox', { name: 'Enter phone number' });
-  // const email = this.page.getByRole('textbox', { name: 'Enter email' });
-  // const password = this.page.getByRole('textbox', { name: 'Password' });
-  // const notification = this.page.getByText('Successfully registered the');
-
   const user = {
     fname: faker.person.fullName(),
     lname: faker.person.lastName(),
@@ -25,7 +16,6 @@ async function user(page: Page, overrides = {}) {
   await page.goto('https://qa-practice.netlify.app/bugs-form/')
   return user
 }
-
 
 test('Successful registration', async ({ page }) => {
   const bugForm = await user(page)
